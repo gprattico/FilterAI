fa1af8e24b3d0c06db71ced20365880099244d3b
Giovanni <gprattico95@gmail.com>
2019-03-26 15:16:30 -0400

    Change regex, so now the vocabulary is clean

--- main.py
+++ main.py
@@ -16,9 +16,18 @@ def trainAI():
 
     for file in os.scandir('./data/train'):
         f = open(file,'r')
-        fileVocab = (re.split('\[\^a-zA-Z\]', f.read()))[0].split(" ")
+        #vocabTemp = (re.split('[^a-zA-Z]', f.read()))[0].split(" ")
+        vocabTemp = re.split('[^a-zA-Z]', f.read())
+        vocabTemp2 = list(filter(('').__ne__, vocabTemp))
+
+        #lowercase for all elements
+        fileVocab = [x.lower() for x in vocabTemp2]
+
+        print(fileVocab)
 
         globalVocab = mergeVocab(globalVocab, fileVocab)
+
+        #print the file name as the program reads the file
         print(file)
         f.close()
 
